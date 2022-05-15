42a41e2a993af5e7a688f75501a9b6ffa7364796
RichardAlvin <richardalvinpratama8@gmail.com>
2022-05-15 22:11:28 +0700

    add flask server and friend and group API

--- CloudComputing/backend/app.js
+++ CloudComputing/backend/app.js
@@ -13,6 +13,9 @@ require('./utils/firestore')
 //connect dotenv
 require("dotenv").config();
 
+//connect with flask
+const request = require('request');
+
 app.use(express.json())
 app.use(express.static('public'));
 
@@ -25,6 +28,15 @@ app.use('/user/feedback',feedback)
 app.get('/', (req, res) => {
   res.send('index')
 })
+
+app.get('/home', function(req, res) {
+  request('http://127.0.0.1:5000/speech', function (error, response, body) {
+      console.error('error:', error); // Print the error
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the data received
+      res.send(body); //Display the response on the website
+    });      
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- CloudComputing/backend/utils/firestore.js
+++ CloudComputing/backend/utils/firestore.js
@@ -1,13 +1,17 @@
 const admin = require('firebase-admin')
+// const { getStorage } = require('firebase-admin/storage');
 
 var serviceAccount = require("../bangkitproject-348609-firebase-adminsdk-kqvha-2d088eeeed.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bangkitproject-348609-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  databaseURL: "https://bangkitproject-348609-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  // storageBucket: process.env.CLOUD_BUCKET
 });
 
 const db = admin.database();
+// const bucket = getStorage().bucket();
 
 module.exports = db
+// module.exports = bucket
 
