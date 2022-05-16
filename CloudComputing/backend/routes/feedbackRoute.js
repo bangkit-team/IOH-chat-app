0dd211004fdab9846ffecfe25ccaf692cd7b9eea
galihsuks <galih8.4.2001@gmail.com>
2022-05-16 20:26:57 +0700

    tambah feedback

--- CloudComputing/backend/routes/feedbackRoute.js
+++ CloudComputing/backend/routes/feedbackRoute.js
@@ -3,7 +3,18 @@ const router = express.Router()
 const db = require('../utils/firestore')
 
 router.post('/:id_user', (req,res) =>{
+    const { id_user } = req.params;
+    const { feedback } = req.body.feedback;
+    if (!feedback) {
+        res.status(500).send({ message: "Belum ada feedback" })
+    }
 
+    const feedbackRef = db.ref('/feedbacks');
+    feedbackRef.child(id_user).set({
+        feedback: req.body.feedback
+    })
+
+    res.status(200).send({ message: `Feedback kamu: ${feedback}`});
 })
 
 module.exports = router
