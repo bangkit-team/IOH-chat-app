d00c5e3909dad698666df342f9baff0a27a222b8
Richard Alvin Pratama <92497609+RichardAlvin@users.noreply.github.com>
2022-05-16 20:40:11 +0700

    Tambah timestamp

--- CloudComputing/backend/routes/feedbackRoute.js
+++ CloudComputing/backend/routes/feedbackRoute.js
@@ -6,12 +6,16 @@ router.post('/:id_user', (req,res) =>{
     const { id_user } = req.params;
     const { feedback } = req.body.feedback;
     if (!feedback) {
-        res.status(500).send({ message: "Belum ada feedback" })
+        res.status(400).send({ message: "Belum ada feedback" })
     }
+    
+    const date = new Date();
+    const dateToday = date.getDate()+'-'+date.getMonth()+'-'+date.getFullYear()
 
     const feedbackRef = db.ref('/feedbacks');
     feedbackRef.child(id_user).set({
-        feedback: req.body.feedback
+        feedback: req.body.feedback,
+        timestamp: dateToday
     })
 
     res.status(200).send({ message: `Feedback kamu: ${feedback}`});
