f4d9d92d18fbb91eb85e9cb459edc39921bfb21d
Aziszy <abdul.azizz@gmail.com>
2022-05-19 01:53:07 +0700

    update: update isi file sentence.txt

--- Machine Learning/code/text generation/collecting_data.py
+++ Machine Learning/code/text generation/collecting_data.py
@@ -1,28 +1,42 @@
+import csv
 import os
-
-import pandas as pd
 
 
 def load_data(path):
-    sentences = list()
+    sentence = list()
 
-    df = pd.read_csv(path)
-    sentences.append(df.Indonesia)
+    with open(path, encoding="utf-8") as f:
+        csv_reader = csv.reader(f)
+        next(csv_reader)
 
-    return sentences
+        for row in csv_reader:
+            sentence.append(row[1])
+
+    return sentence
 
 
-def convert_to_txt(path, sentences):
-    with open(path, "w", encoding="utf-8") as f:
-        for row in sentences:
-            f.write(row)
-            f.write("\n")
+def convert_to_txt(path, sentence):
+    mode = "w"
+    if not os.path.exists(path):
+        mode = "w"
+    else:
+        mode = "a"
+
+    with open(path, mode, encoding="utf-8") as f:
+        for row in sentence:
+            if len(row.split(" ")) >= 20:
+                f.write(" ".join(row.split(" ")[:20]))
+                f.write("\n")
+            elif len(row.split(" ")) >= 10:
+                f.write(row)
+                f.write("\n")
 
 
 def main():
     TXT_DIR = "datasets/text generation/sentence.txt"
-    FILE_DIR = "datasets/translate sentence/result/eng-ind.csv"
-    files = load_data(FILE_DIR)
+    FILE_DIR = ["datasets/translate sentence/result/eng-ind.csv",
+                "datasets/feedbacks/data_feedbacks.csv"]
+    files = [load_data(dir) for dir in FILE_DIR]
 
     for file in files:
         convert_to_txt(TXT_DIR, file)
