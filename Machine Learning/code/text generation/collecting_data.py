74f856a6455776104665e9727fa897ac798015cb
Aziszy <abdul.azizz@gmail.com>
2022-05-19 00:10:12 +0700

    update: update isi file sentence.txt

--- Machine Learning/code/text generation/collecting_data.py
+++ Machine Learning/code/text generation/collecting_data.py
@@ -1,38 +1,28 @@
-import csv
 import os
+
+import pandas as pd
 
 
 def load_data(path):
-    sentence = list()
+    sentences = list()
 
-    with open(path, encoding="utf-8") as f:
-        csv_reader = csv.reader(f)
-        next(csv_reader)
+    df = pd.read_csv(path)
+    sentences.append(df.English)
 
-        for row in csv_reader:
-            sentence.append(row[1])
-
-    return sentence
+    return sentences
 
 
-def convert_to_txt(path, sentence):
-    if not os.path.exists(path):
-        with open(path, "w", encoding="utf-8") as f:
-            for row in sentence:
-                f.write(row)
-                f.write("\n")
-    else:
-        with open(path, "a", encoding="utf-8") as f:
-            for row in sentence:
-                f.write(row)
-                f.write("\n")
+def convert_to_txt(path, sentences):
+    with open(path, "w", encoding="utf-8") as f:
+        for row in sentences:
+            f.write(row)
+            f.write("\n")
 
 
 def main():
     TXT_DIR = "datasets/text generation/sentence.txt"
-    FILE_DIR = ["datasets/translate sentence/result/eng-ind.csv",
-                "datasets/feedbacks/data_feedbacks.csv"]
-    files = [load_data(dir) for dir in FILE_DIR]
+    FILE_DIR = "datasets/translate sentence/result/eng-ind.csv"
+    files = load_data(FILE_DIR)
 
     for file in files:
         convert_to_txt(TXT_DIR, file)
