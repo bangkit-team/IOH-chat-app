7f9aa83d2a4cd188c326b36354011641f6f2d5c2
Vanny Ezhaan Nur Sandika <vannyezhaa@gmail.com>
2022-06-01 22:19:06 +0700

    change transfer learning model

--- Machine Learning/code/chat_non_chat/predict.py
+++ Machine Learning/code/chat_non_chat/predict.py
@@ -5,9 +5,11 @@ model = tf.keras.models.load_model("saved_models")
 model.summary()
 
 
+
 path = "/home/vannyezha/project/IOH-chat-app/Machine Learning/code/chat_non_chat/src/IOH.png"
-img = tf.keras.preprocessing.image.load_img(path, target_size=(224, 224))
+img = tf.keras.preprocessing.image.load_img(path,color_mode="rgb", target_size=(224, 224))
 x = tf.keras.preprocessing.image.img_to_array(img)
+x /= 255
 
 x = np.expand_dims(x, axis=0)
 images = np.vstack([x])
